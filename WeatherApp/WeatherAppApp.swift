4423f6f7e29caeba130b7d7d576c20c2d9359749
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-04-22 11:18:20 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yogesh Bhutkar on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
